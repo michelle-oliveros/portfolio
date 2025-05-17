8cda64731d4abdde835bf7abcd2b8018cb83c786
michelle-oliveros <mchllolvrsxx@gmail.com>
2025-05-17 11:27:51 +0800

    Final Edit

--- script.js
+++ script.js
@@ -154,3 +154,26 @@ balls.forEach((el, i, ra) => {
     }
   );
 });
+
+const toggle = document.getElementById('mode-toggle');
+const body = document.body;
+
+toggle.addEventListener('change', function() {
+    if (this.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+
+// check for saved user preference, if any, on load of the website
+document.addEventListener('DOMContentLoaded', (event) => {
+    const darkMode = localStorage.getItem('darkMode');
+    
+    if (darkMode === 'enabled') {
+        body.classList.add('dark-mode');
+        toggle.checked = true;
+    }
+});
